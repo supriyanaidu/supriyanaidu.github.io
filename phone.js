82ece6405ca7dd58da6cbf23ce1cfa3c277ba37f
supriyanaidu <32039542+supriyanaidu@users.noreply.github.com>
2019-01-29 22:08:47 -0700

    Phone Dialer

---
+++ phone.js
@@ -0,0 +1,25 @@
+$(document).ready(function() { // do this when the document is loaded
+	$("#idContentDialer").show(); // show the element with ID "element"
+	$("#idContentNewContact").hide(); // hide the element with ID "otherElement"
+	$("#idContentContactList").hide();
+});
+$("#dialerTab").click(function() { // when "button_id" is clicked
+$("#idContentDialer").show(); // show the element with ID "element"
+$("#idContentNewContact").hide(); // hide the element with ID "otherElement"
+$("#idContentContactList").hide();
+});
+$("#contactListTab").click(function() { // when "button_id" is clicked
+$("#idContentDialer").hide(); // show the element with ID "element"
+$("#idContentNewContact").hide(); // hide the element with ID "otherElement"
+$("#idContentContactList").show();
+});
+$("#newContactTab").click(function() { // when "button_id" is clicked
+$("#idContentDialer").hide(); // show the element with ID "element"
+$("#idContentNewContact").show(); // hide the element with ID "otherElement"
+$("#idContentContactList").hide();
+});
+function populateDialer(dialerNumber) {
+	var currentNumber = document.getElementByID("dialerPrompt").value;
+	currentNumber = currentNumber + dialerNumber;
+	document.getElementByID("dialerPrompt").value=currentNumber;
+}
